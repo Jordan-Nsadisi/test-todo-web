78a5899548f735e90e18bb5ba0d4f957af24da08
Jordan Nsadisi <astro17nsadisi@gmail.com>
2025-12-31 07:49:46 +0100

    add apiRoutes

---
+++ utils/apiRoutes.ts
@@ -0,0 +1,14 @@
+
+
+export const auth = {
+   register: "/register",
+   login: "/login",
+   logout: "/logout",
+}
+
+export const tasks = {
+   get: "/",
+   create: "/",
+   update: "/",
+   delete: "/",
+}
