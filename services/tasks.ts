be2f5e810d4d91065ed1ec671be9f94120264d59
Jordan Nsadisi <astro17nsadisi@gmail.com>
2026-01-01 02:27:40 +0100

    implement tasksServices and create task service

---
+++ services/tasks.ts
@@ -0,0 +1,17 @@
+import { API_ENDPOINTS } from "@/utils/constant";
+import API from "./api";
+import { tasks } from "@/utils/apiRoutes";
+import { TaskFormData } from "@/types";
+
+
+export const tasksServices = {
+
+   async creatTask(task: TaskFormData): Promise<any> {
+      const response = await API.post<TaskFormData>(
+         `${API_ENDPOINTS.task}${tasks.create}`,
+         task
+      );
+      return response.data;
+   },
+
+}
